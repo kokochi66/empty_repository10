11d1b0c8d23606954a53e4afa7430bea998f42f9
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-07-28 19:21:41 +0900

    StudyFlim으로 리파지토리 이동, 정리

--- sdk/sdk/src/main/java/com/github/kokochi/link/developers/sdk/login/api/LoginConstants.java
+++
@@ -1,6 +0,0 @@
-package com.github.kokochi.link.developers.sdk.login.api;
-
-public class LoginConstants {
-    public static String ISSUE_ACCESS_TOKEN = "/oauth2/v2.1/token";
-    public static String GET_USER_PROFILE = "/v2/profile";
-}
